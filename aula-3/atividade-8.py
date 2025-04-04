f109f99132231485c99ccbb92b870fec549aaf28
Tkr7-Alice <thomas.kirmeier@gmail.com>
2025-04-03 21:19:51 -0300

    l

---
+++ aula-3/atividade-8.py
@@ -0,0 +1,8 @@
+valor_hora = 0
+total_horas = 0
+
+valor_hora = int(input("Quanto você ganha por hora:"))
+total_horas = int(input("Informe o toral de horas trabalhadas"))
+
+total_trabalhado = valor_hora * total_horas
+print("O valor toral do salário é : R$",total_trabalhado)
