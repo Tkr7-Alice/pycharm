79712d08d8e1da0ce658f65a0865446ea8538e22
Tkr7-Alice <thomas.kirmeier@gmail.com>
2025-04-03 21:38:12 -0300

    l

--- aula-3/atividade 9.py
+++
@@ -1,4 +0,0 @@
-far = float(input("Type a number in fahrenheit degreed; "))
-
-celsius = 5 * ((far-32) / 9)
-print("Your fahrenheit degres in Celsius are equal to:", celsius)
--- aula-3/atividade-6.py
+++
@@ -1,5 +0,0 @@
-raio = 0
-
-raio = int(input("Digite um valor:"))
-area = (raio * raio) * 3.14
-print("O reseultado é igual a:", area)
