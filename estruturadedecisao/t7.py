7d5a4a05f2c6d9e2e5a11571344936c722ed5978
Tkr7-Alice <thomas.kirmeier@gmail.com>
2025-04-08 20:08:03 -0300

    lAtualização

---
+++ estruturadedecisao/t7.py
@@ -0,0 +1,9 @@
+n1 = float(input("Primeiro número:"))
+n2 = float(input("Segundo número:"))
+n3 = float(input("Terceiro número:"))
+
+maior = max(n1,n2,n3)
+menor = min(n1,n2,n3)
+
+print("O mamior número é:", maior)
+print("O menor número é:", menor)
