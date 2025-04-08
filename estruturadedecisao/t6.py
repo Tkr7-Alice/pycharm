e01d4d847c6c0a6348226323d3ab3b41f4c98392
Tkr7-Alice <thomas.kirmeier@gmail.com>
2025-04-08 19:41:37 -0300

    lAtualização

--- estruturadedecisao/t6.py
+++ estruturadedecisao/t6.py
@@ -1,11 +1,14 @@
+n1 = 0
+n2 = 0
+n3 = 0
 n1 = float(input("Primeiro número:"))
 n2 = float(input("Segundo número:"))
 n3 = float(input("Terceiro número:"))
-
-maior = n1
-if n2 > maior:
-    maior = n2
-if n3 > maior:
-    maior = n3
-
-print("O maior número é:", maior)
+if (n1 == n2) and (n1 == n3):
+    print('Os números são iguais')
+elif (n1 > n2) and (n1 > n3):
+    print(n1, "O maior é o número")
+elif (n2 > n3):
+    print(n2, "O maior é o número")
+else:
+    print(n3, "O maior é o número")
