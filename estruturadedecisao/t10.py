8050e1acc6441d238ea39a1ca6f9ed341a0cbb7f
Tkr7-Alice <thomas.kirmeier@gmail.com>
2025-04-09 19:55:51 -0300

    lAtualização

---
+++ estruturadedecisao/t10.py
@@ -0,0 +1,10 @@
+turno = input("Qual turno você estuda?")
+
+if (turno == "M"):
+    print("Bom dia!")
+elif (turno == "V"):
+    print("Boa tarde!")
+elif (turno == "N"):
+    print("Boa noite!")
+else:
+    print("Valor inválido! Use M, V ou N.")
