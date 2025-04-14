1c0cedde504149cb756ca2070d094150c58d2121
Tkr7-Alice <thomas.kirmeier@gmail.com>
2025-04-14 19:29:26 -0300

    lAtualização

--- lista/t1.py
+++
@@ -1,9 +0,0 @@
-lista = []
-i = 0
-
-for i in range(0, 5):
-    lista.append(int(input("Digite um número inteiro:")))
-
-lista.sort()
-print(lista)
-
--- lista/t2.py
+++
@@ -1,8 +0,0 @@
-vetor = []
-i = 0
-
-for i in range(10):
-    vetor.append(int(input("Digite um número inteiro:")))
-
-vetor.reverse()
-print(vetor)
