075fa078ff9bc0f786eb87e1b0afe31b4aba5d08
Tkr7-Alice <thomas.kirmeier@gmail.com>
2025-04-14 21:48:05 -0300

    Atualização

--- estrutura-4/funcoes/t3.py
+++
@@ -1,9 +0,0 @@
-def calcular(base, expoente, multiplicador):
-    return (base ** expoente) * multiplicador, base + expoente + multiplicador
-
-base = float(input("Base: "))
-expoente = float(input("Expoente: "))
-multiplicador = float(input("Multiplicador: "))
-resultado, soma = calcular(base, expoente, multiplicador)
-
-print(f"Resultado: {resultado}, Soma: {soma}")
